4d0ecd66b3954071a07ca2615f895b043217a165
Sher Chowdhury <sher.chowdhury@ibm.com>
2020-02-12 11:42:22 +0000

    Just after running 'operator-sdk add api --api-version=cache.codingbee.net/v1alpha1 --kind=MySQL'

---
+++ pkg/apis/cache/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the cache v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=cache.codingbee.net
+package v1alpha1
---
+++ pkg/apis/cache/v1alpha1/mysql_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MySQLSpec defines the desired state of MySQL
+type MySQLSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// MySQLStatus defines the observed state of MySQL
+type MySQLStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// MySQL is the Schema for the mysqls API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=mysqls,scope=Namespaced
+type MySQL struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MySQLSpec   `json:"spec,omitempty"`
+	Status MySQLStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// MySQLList contains a list of MySQL
+type MySQLList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MySQL `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&MySQL{}, &MySQLList{})
+}
